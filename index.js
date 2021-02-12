eac49c9266d27120ebbd4e21a44532f4a746c7eb
Artem <artskadin@gmail.com>
2021-02-12 19:15:30 +0300

    change index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('hello')
+  res.send('This shit is working')
 })
 
 app.listen(3000, () => console.log('server is runnig on port 3000'))
