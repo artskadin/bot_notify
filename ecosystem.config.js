f6e1a7741d6581754c675dea1edff525b548c169
Artem <artskadin@gmail.com>
2021-02-15 15:32:23 +0300

    change process manager

---
+++ ecosystem.config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  apps : [{
+    name        : "APPool",
+    script      : "./src/index.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+       "NODE_ENV": "production"
+    }
+  },{
+    name        : "APGrowFinance",
+    script      : "./src/bots/APGrowFinance/index.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+       "NODE_ENV": "production"
+    }
+  },
+  {
+    name        : "APNyseBrok",
+    script      : "./src/bots/APNyseBrok/index.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+       "NODE_ENV": "production"
+    }
+  },
+  {
+    name        : "APBenefitFX",
+    script      : "./src/bots/APBenefitFX/index.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+       "NODE_ENV": "production"
+    }
+  },
+  {
+    name        : "APTryton",
+    script      : "./src/bots/APTryton/index.js",
+    watch       : true,
+    env: {
+      "NODE_ENV": "development",
+    },
+    env_production : {
+       "NODE_ENV": "production"
+    }
+  }]
+}
